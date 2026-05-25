465fe218b1e6544bbb141b58a02157e667cd048c
Dylan Barwick <dylan.barwick01@student.wandw.ac.nz>
2026-05-26 10:08:03 +1200

    Added Fields,Properties,Constructor and display customer details method to the customer class

--- Customer.cs
+++ Customer.cs
@@ -6,7 +6,40 @@ using System.Threading.Tasks;
 
 namespace MovieRentalApp
 {
-    internal class Customer
+    public class Customer
     {
+
+        //Fields
+        private string custPassword;
+        private string custLogin;
+        private string custName;
+        private string custPhone;
+        private string custEmail;
+        
+        //Properties 
+        public string CustPassword { get { return custPassword; } set { custPassword = value; } }
+        public string CustLogin { get { return custLogin; } set { custLogin = value; } }
+        public string CustName { get { return custName; } set {custName = value; } }
+        public string CustPhone { get { return custPhone; } set {custPhone = value; } }
+        public string CustEmail { get { return custEmail; } set {custEmail = value; } }
+
+        //Constructor
+        public Customer(string custPassword, string custLogin, string custName, string custPhone, string custEmail )
+        {
+            CustPassword = custPassword;
+            CustLogin = custLogin;
+            CustName = custName;
+            CustPhone = custPhone;
+            CustEmail = custEmail;
+        }
+
+        //Methods
+        public void DisplayCustomerDetails()
+        {
+            Console.WriteLine($"\nName: \t{CustName} \nPhone: \t{CustPhone}\nEmail: \t{CustPhone}");
+        }
+
+
+
     }
 }
